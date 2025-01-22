5547e90ab531041048c795e392287a7ee81d508a
Benjamin <benjamin.souiket@gmail.com>
2025-01-22 13:52:28 +0100

    test

--- index.js
+++ index.js
@@ -30,8 +30,7 @@ app.use(session({
   resave: true,
   saveUninitialized: true,
   cookie: {
-    secure: true,
-    sameSite: 'lax',
+    secure: false,
     maxAge: (1000 * 60) * (60 * 4)
   },
 })
